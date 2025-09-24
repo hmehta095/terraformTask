0f392bcda65f2ba209cb46b396ef7e3abfc72b59
Himanshu Mehta <himu@Himanshus-MacBook-Air.local>
2025-09-23 22:11:32 -0400

    module for qa environment

---
+++ qa/main.tf
@@ -0,0 +1,19 @@
+module "dev" {
+    source = "../modules/keyvault_and_storage"
+    
+    environment ={
+        name           = "qa"
+    }
+storage_buckets = [
+    {
+      name               = "my-storage-lrs"
+      region             = "ca-central-1"
+      public_access      = false
+    },
+    {
+      name               = "my-storage-ragrs"
+      region             = "ca-east-1"
+      public_access      = true
+    }
+  ]
+}
