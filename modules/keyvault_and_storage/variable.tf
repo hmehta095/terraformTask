3d2c1467fde7d9248ca0a2e02190a46820e9adfc
Himanshu Mehta <himu@Himanshus-MacBook-Air.local>
2025-09-25 00:19:57 -0400

    typo fix and adding prod & nonprod key

--- modules/keyvault_and_storage/variable.tf
+++ modules/keyvault_and_storage/variable.tf
@@ -1,10 +1,15 @@
-variable "env" {
+variable "environment" {
   description = "Environment tagging and validating env"
   type        = string
   validation {
-    condition     = contains(["dev", "qa", "stg", "prd"], var.environment)
+    condition     = contains(["dev", "qa", "stg", "prd"], var.environment.name)
     error_message = "The env variable must be one of dev, qa, stg, prd"
   }
+}
+
+variable "kms_key_alias" {
+  description = "Alias for the KMS key (Key Vault equivalent)"
+  type        = string
 }
 
 variable "storage_buckets" {
