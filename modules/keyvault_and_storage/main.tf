a6beb22262da80f7fe68f238258f703786bf75a0
Himanshu Mehta <himu@Himanshus-MacBook-Air.local>
2025-09-25 01:09:38 -0400

    adding ragr config in module variable.tf

--- modules/keyvault_and_storage/main.tf
+++ modules/keyvault_and_storage/main.tf
@@ -2,11 +2,55 @@ resource "aws_kms_key" "vault" {
   description             = "KMS key for ${var.kms_key_alias}"
   deletion_window_in_days = 7
   tags = {
-    env = var.env
+    env = var.environment.name
   }
 }
 
 resource "aws_kms_alias" "vault_alias" {
   name          = "alias/${var.kms_key_alias}"
   target_key_id = aws_kms_key.vault.id
+}
+
+resource "aws_s3_bucket" "lrs" {
+  provider = var.storage_buckets_lrs.provider
+  region   = var.storage_buckets_lrs.region
+  bucket   = var.storage_buckets_lrs.name
+
+  acl = "var.storage_buckets_lrs.access "
+
+  tags = {
+    env = var.environment.name
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "lrs" {
+  provider = aws.central
+  bucket   = aws_s3_bucket.lrs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "ragr" {
+  provider = var.storage_buckets_ragr.provider
+  region   = var.storage_buckets_ragr.region
+  bucket   = var.storage_buckets_ragr.name
+
+  acl = "var.storage_buckets_ragr.access "
+
+  tags = {
+    env = var.environment.name
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "ragr" {
+  provider = aws.central
+  bucket   = aws_s3_bucket.ragr.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
